572ce3cacb386e4ee878a1114cf4f862658bc86a
Alexander Aonerud <alexandersonerud@hotmail.com>
2019-11-21 15:23:25 +0100

    master program

--- WorkPlace/Program.cs
+++ WorkPlace/Program.cs
@@ -6,7 +6,7 @@ namespace WorkPlace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Master");
         }
     }
 }
