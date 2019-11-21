2196444786bb6b7f30270422b703a2af0ca8518c
Alexander Aonerud <alexandersonerud@hotmail.com>
2019-11-21 15:24:14 +0100

    Developer program

--- WorkPlace/Program.cs
+++ WorkPlace/Program.cs
@@ -6,7 +6,7 @@ namespace WorkPlace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Master");
+            Console.WriteLine("Developer");
         }
     }
 }
